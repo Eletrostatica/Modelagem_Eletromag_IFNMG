18c9a5da8526c8b5c6c859a2ab6ecaed2ff24fb8
Eletrostatica <105019092+Eletrostatica@users.noreply.github.com>
2022-07-03 21:25:53 -0300

    Update Eletrostatica.py

--- Eletrostatica.py
+++ Eletrostatica.py
@@ -1,5 +1,5 @@
 import math
-from algebravetorial import vet_uni,inserir_pv_cart
+from trab_eletromag_analisevet import vet_uni,inserir_pv_cart
 
 ################### Instruções de Uso ##################
 
