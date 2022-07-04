a72d223adcf27bb7e7376a9a140985b1d3a8ec98
Eletrostatica <105019092+Eletrostatica@users.noreply.github.com>
2022-07-04 10:58:57 -0300

    Update Eletrostatica.py

--- Eletrostatica.py
+++ Eletrostatica.py
@@ -16,6 +16,9 @@ from trab_eletromag_analisevet import vet_uni,inserir_pv_cart
 
 # Forca(carga1(escalar), posição da carga1(vetor), carga2(escalar), posição da carga2(vetor))
 # Restorna o vetor força, entre a carga1 e a carga 2
+# Exemplo :Duas cargas pontuais de 1mC e -2mC, estão localizadas em (3,2,-1) e
+# (-1,-1,4).Calcule a força elétrica sobre uma carga de 10nC, localizada em (0,3,1)
+# e intensidade do campo elétrico nesse ponto.
 
 # Campo_eletrico(carga1, posição da carga1, posição de referência)
 # retorna o vetor campo elétrico, na posição de referência
